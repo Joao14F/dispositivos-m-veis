7f3e50002926a139d1f179561114647a434feba2
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-06-10 10:31:52 -0300

    Funciona pelo tunnel

--- ProjetoTop/App.js
+++ ProjetoTop/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Mega top</Text>
+      <Text>Fedoca Lindao</Text>
       <StatusBar style="auto" />
     </View>
   );
